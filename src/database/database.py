46db326fe6812b8b514b34076679263ad54b7f45
hitesh3121 <hitesh.fluxbyte@gmail.com>
2023-10-13 11:30:04 +0530

    remove

--- src/database/database.py
+++ src/database/database.py
@@ -1,29 +1,30 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pathlib import Path
 from sqlalchemy import create_engine, orm
 from sqlalchemy.ext.declarative import declarative_base
 
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = Path('.')/'.env'
+# load_dotenv(dotenv_path=env_path)
 
 
 class Development_Settings:
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER = os.getenv('POSTGRES_SERVER')
-    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    # POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    # POSTGRES_SERVER = os.getenv('POSTGRES_SERVER')
+    # POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    # POSTGRES_DB = os.getenv('POSTGRES_DB')
+    # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD = "mihir"
+    POSTGRES_SERVER = "localhost"
+    POSTGRES_PORT = 5432
+    POSTGRES_DB = "Todo"
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
-class Production_Settings:
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER = os.getenv('POSTGRES_SERVER')
-    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-    POSTGRES_DB = os.getenv('POSTGRES_DB')
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# s//{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Development_Settings()
 
